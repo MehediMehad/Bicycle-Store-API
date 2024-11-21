b987ea14ef7144de17218f38977e57ff15d3c364
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-11-21 21:11:23 +0600

    Project besic setup server is running

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express';
+import config from './app/config';
+import cors from 'cors';
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// applications routs
+const getAController = (req: Request, res: Response) => {
+    res.send(`The bicycle is moving at a speed of ${config.port}`);
+};
+
+app.get('/', getAController);
+
+export default app;

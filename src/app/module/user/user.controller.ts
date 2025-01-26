b98f6804a8e1523f004b326827ec01b30de64d31
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-01-26 21:20:48 +0600

    Send Response utils  create

--- src/app/module/user/user.controller.ts
+++ src/app/module/user/user.controller.ts
@@ -1,13 +1,15 @@
 import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { StatusCodes } from 'http-status-codes';
 import { UserServices } from './user.service';
 
 const createUser = catchAsync(async (req, res) => {
     const result = await UserServices.createUserIntoDB(req.body);
-    res.status(201).json({
-        status: 'success',
-        data: {
-            result
-        }
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: 'OfferedCourse fetched successfully',
+        data: result
     });
 });
 

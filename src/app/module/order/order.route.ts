8f961da4a6363a620fb4ea31333e41c3c4abe671
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-01-27 18:34:44 +0600

    Verify token, authorization  using auth

--- src/app/module/order/order.route.ts
+++ src/app/module/order/order.route.ts
@@ -1,9 +1,14 @@
+import auth from '../../Middleware/auth';
 import { orderController } from './order.controller';
 import express from 'express';
 
 const router = express.Router();
 
 router.post('/', orderController.createOrder);
-router.get('/revenue', orderController.getAndCalculateRevenue);
+router.get(
+    '/revenue',
+    auth('customer'),
+    orderController.getAndCalculateRevenue
+);
 
 export const OrderRouter = router;
